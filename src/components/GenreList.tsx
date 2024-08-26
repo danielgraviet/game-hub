9ca0a0939433372b58cb87ce716c0ff3074654e7
danielgraviet <dgravs5@gmail.com>
2024-08-25 20:33:30 -0600

    ship genres with app

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,4 @@
-import {
-  HStack,
-  List,
-  ListItem,
-  Image,
-  Text,
-  Spinner,
-  Button,
-  Heading,
-} from "@chakra-ui/react";
+import {HStack, List,ListItem,Image,Text,Spinner,Button,Heading,} from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
